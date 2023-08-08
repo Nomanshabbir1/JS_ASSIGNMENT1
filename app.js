6b45db61fbd497e228c7bed45ff9e706afad8f5a
Nomanshabbir <135643492+Nomanshabbir1@users.noreply.github.com>
2023-08-08 12:00:41 -0700

    JS 1assignment done

---
+++ app.js
@@ -0,0 +1,16 @@
+alert("Error! please enter a valid Password")
+
+alert("welcome to JS world... \nhappy coding")
+
+alert("Welcome to JS Land")
+
+alert("Happy coding")
+
+alert("Hello i can run JS through my web browser console")
+
+console.log("alert")
+
+
+
+
+

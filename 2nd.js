ed13bbe6ed291942d65a73d644d2f7215d9676ed
Nomanshabbir <135643492+Nomanshabbir1@users.noreply.github.com>
2023-08-08 12:29:20 -0700

    2nd assignment done

---
+++ 2nd.js
@@ -0,0 +1,30 @@
+var username = ("");
+
+var myname =("NOMAN SHABBIR")
+
+var message=("hello world")
+
+alert(message)
+
+var fullName= ("Noman Shabbir")
+alert(fullName)
+
+var age=("24 years old")
+alert(age)
+
+var certification=("WEB N APP DEVELOPMENT")
+alert(certification)
+
+var pizza=("pizza\npizz\npiz\npi\np")
+alert(pizza)
+
+var myemail =("my email address is muhammadnoman1l101@gmail.com")
+alert(myemail)
+
+var book=("A samter way to learn Javascript")
+alert(book)
+
+document.write("<h1>Yah! I can write HTML content through Javascript</h1>")
+
+var design=("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”")
+alert(design)

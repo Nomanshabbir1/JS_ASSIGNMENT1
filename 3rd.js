9481fd46a5250e98563cd525a981246eecd9eb22
Nomanshabbir <135643492+Nomanshabbir1@users.noreply.github.com>
2023-08-09 08:35:45 -0700

    assignment 3 and 4 done

---
+++ 3rd.js
@@ -0,0 +1,28 @@
+
+// var age14=("I am 24 years old")
+// alert(age14)
+
+// var birthyear1999=("My birthyear is 1999\n  Data type of my declared variable is number")
+// alert(birthyear1999)
+
+// var vistorname=("Noman shabbir")
+// var clothes=("ordered")
+// var quantity=("5 T-shirts")
+// var title=("on XYZ clothing store")
+
+// alert(vistorname + " "+ clothes +" "+ quantity +" "+ title)
+
+
+
+
+// var visitorName = "John Doe", productTitle = "T-shirt", quantity = 5;
+
+// Legalvariable name { name, birth_year, money$, age19, Residence}
+
+// illegal variable name { 19age, birth-year, money $, class, 2residence}
+
+document.write("<h1>Rules for naming JS variables</h1>")
+document.write("<p>Variable names can only contain $, numbers, _. For example $my_1stVariable</p>")
+document.write("<p>Variables must begin with a letter,$ or _ . For example $name, _name or name</p>")
+document.write("<p>Variable names are case Sensitive</p>")
+document.write("<p>Variable names should not be JS Keywords</p>")
